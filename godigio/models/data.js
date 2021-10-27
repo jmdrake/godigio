58e563ba6eca0fa3c701201eaf76780d22d3be51
jmdrake <johnmdrake@gmail.com>
2021-10-27 14:55:38 -0500

    Create data.js

---
+++ godigio/models/data.js
@@ -0,0 +1,9 @@
+/*
+var fanhub = new PouchDB("https://admin:8a7d03517aed@godigio.smileupps.com/fanhub");
+var profiles = new PouchDB("https://admin:8a7d03517aed@godigio.smileupps.com/profiles");
+var posts = new PouchDB("https://admin:8a7d03517aed@godigio.smileupps.com/posts");
+*/
+
+var fanhub = new PouchDB("fanhub");
+var profiles = new PouchDB("profiles");
+var posts = new PouchDB("posts");
